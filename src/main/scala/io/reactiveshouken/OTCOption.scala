7c1f2af4574ba132b786834ade7a47fb362ca995
David Stevenson <david.35472@gmail.com>
2020-07-17 22:18:35 +0900

    add quantity to model

--- src/main/scala/io/reactiveshouken/OTCOption.scala
+++ src/main/scala/io/reactiveshouken/OTCOption.scala
@@ -1,10 +1,12 @@
 package io.reactiveshouken
 
-
 object OTCOption {
 
-  def apply(contractId: String): Unit = {
+  class Quantity(val value: Int)
+
+  def apply(contractId: String, instrument: String, quantity: Quantity): Unit = {
     require(contractId.nonEmpty, "contractId is required.")
+    require(quantity.value > 0, "quantity must be greater than 0")
   }
 
 }
--- src/test/scala/io/reactiveshouken/OTCOptionSpec.scala
+++ src/test/scala/io/reactiveshouken/OTCOptionSpec.scala
@@ -5,11 +5,18 @@ import org.scalatest.wordspec.AnyWordSpecLike
 
 class OTCOptionSpec extends ScalaTestWithActorTestKit() with AnyWordSpecLike {
 
+  import OTCOption._
+
   "The OTC Option contract" should {
 
     "throw an IllegalArgumentException when 'ContractId' is empty" in {
-      intercept[IllegalArgumentException] { OTCOption("") }
+      intercept[IllegalArgumentException] { OTCOption("", "TOYOTA", new Quantity(10000)) }
+    }
+
+    "throw an IllegalArgumentException when 'Quantity' is zero" in {
+      intercept[IllegalArgumentException] { OTCOption("123456789", "TOYOTA", new Quantity(0)) }
     }
 
   }
+
 }
